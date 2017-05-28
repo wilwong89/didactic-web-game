f4da3e10a7067df31ca9a1af7a5bae16112157e4
Ellie May <saberkaze@gmail.com>
2017-05-28 03:01:02 -0400

    Memes for the meme god
    he asked for a meme picture. blame him.

--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import DuckImage from '../assets/Duck.jpg'
+import MemeImage from '../assets/meme.jpg'
 import './HomeView.scss'
 
 export const HomeView = () => (
   <div>
     <h4>Welcome!</h4>
-    <img alt='This is a duck, because Redux!' className='duck' src={DuckImage} />
+    <img alt='This is a meme, because Bryan demanded it!' className='meme' src={MemeImage} />
   </div>
 )
 
